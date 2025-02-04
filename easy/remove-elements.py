d84ab736608d1b5a8c4a65dc23217954c2dcb9cd
Leo <leonardonifinluri@gmail.com>
2025-02-04 15:58:46 +0800

    easy problem

---
+++ easy/remove-elements.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def removeElement(nums: List[int], val: int) -> int:
+    tempList = [99] * len(nums)
+    idx = 0
+    for i in nums:
+        if i == val:
+            continue
+        tempList[idx] = i
+        idx += 1
+    nums[:] = tempList
+    return idx
+
+myList = [3,2,2,3]
+print(removeElement(nums=myList, val=3))
+print(myList)
