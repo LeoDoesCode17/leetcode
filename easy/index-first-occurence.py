01b08b2426b5504631759c1007321d7675b7fc63
Leo <leonardonifinluri@gmail.com>
2025-03-13 15:02:48 +0800

    28. Find the index of the first occurance in a string

---
+++ easy/index-first-occurence.py
@@ -0,0 +1,34 @@
+import unittest
+
+# command to run the test : python -m unittest "path to file relative to current directory"
+
+# Task : find the first index where the needle occurs in the haystack
+# Idea : if given character is same as the needle, slice haystack with the length of the needle starting from the current index
+def solve(haystack, needle):
+    if len(haystack) < len(needle):
+        return -1
+    n = len(haystack)
+    m = len(needle)
+    for i in range(n):
+        if haystack[i] == needle[0]:
+            lastIdx = i + m
+            subArr = haystack[i:lastIdx]
+            if subArr == needle:
+                return i
+    return -1
+
+class TestClass(unittest.TestCase):
+    def first_test(self):
+        result = solve("sadbutsad", "sad")
+        expected = 0
+        self.assertEqual(result, expected)
+    def second_test(self):
+        result = solve("leetcode", "leeto")
+        expected = -1
+        self.assertEqual(result, expected)
+    def third_test(self):
+        result = solve("mississippi", "issip")
+        expected = 4
+        self.assertEqual(result, expected)
+
+print(solve('hello', 'll'))
